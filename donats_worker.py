d052e36a2f7518711ebf762432d9bf2ce08ff1cb
loki <gunlinux@ya.ru>
2026-04-08 22:19:43 +0300

    worker as faststream

--- donats_worker.py
+++ donats_worker.py
@@ -3,9 +3,12 @@ import asyncio
 from donats.handlers import DonatEventHandler
 from donats import settings
 from donats.utils import logger_setup
-from requeue.requeue import Queue
 from requeue.rredis import RedisConnection
 from requeue.sender.sender import Sender
+
+from faststream.rabbit import RabbitBroker
+from requeue.fstream.consumer import RabbitConsumer
+
 
 logger = logger_setup(__name__)
 logger.info('Donats worker service started')
@@ -13,14 +16,19 @@ logger.info('Donats worker service started')
 
 async def main() -> None:
     redis_url: str = settings.donats_redis_url
+    broker = RabbitBroker(settings.rabbit_url, virtualhost=settings.rabbit_vhost)
+
     async with RedisConnection(redis_url) as redis_connection:
-        queue = Queue(name=settings.DONATS_EVENTS, connection=redis_connection)
         sender = Sender(queue_name=settings.TWITCH_OUT, connection=redis_connection)
         donat_handler: DonatEventHandler = DonatEventHandler(
             sender=sender,
             admin='gunlinux',
         )
-        await queue.consumer(donat_handler.on_message)
+        await RabbitConsumer(
+            broker=broker,
+            worker=donat_handler.on_message,
+            queue_name=settings.DONATS_EVENTS,
+        ).consume()
 
 
 if __name__ == '__main__':
